211033dbe3495472385f1ee5d6a6250ccd5b2b4f
胡棕林 <skycoop@163.com>
2022-10-25 16:32:06 +0800

    调整docker在线安装脚本

--- component/docker/online_install.sh
+++ component/docker/online_install.sh
@@ -8,6 +8,23 @@
 ## 
 ################################################################################
 
+# info级别的日志 (String:msg) -> []
+F_log_info(){
+  local _msg="$1"
+  if [[ x"${_msg}" != x"" ]];then
+    echo -e "\a\033[32m[Info] $* \033[0m"
+  fi
+}
+
+# error级别的日志 (String:msg) -> []
+Func_log_err(){
+  local _msg="$1"
+  if [[ x"${_msg}" != x"" ]];then
+    echo -e "\a\033[31m[Err] $* \033[0m"
+  fi
+}
+
+F_log_info "[1] 移除旧的docker"
 sudo yum remove docker \
                   docker-client \
                   docker-client-latest \
@@ -17,14 +34,16 @@ sudo yum remove docker \
                   docker-logrotate \
                   docker-engine
 
+F_log_info "[2] 添加Yum仓库映射" 
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install -y docker-ce docker-ce-cli containerd.io
-systemctl start docker
 
-# 修改docker镜像地址
-mkdir -p /etc/docker
-tee /etc/docker/daemon.json <<-'EOF'
+F_log_info "[3] 安装最新的docker"
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+F_log_info "[4] 修改docker镜像地址"
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
 {
   "data-root": "/home/docker-data",
   "storage-driver": "overlay2",
@@ -37,19 +56,21 @@ tee /etc/docker/daemon.json <<-'EOF'
 }
 EOF
 
-# 开启开机自启动
-systemctl enable docker
+F_log_info "[5] 开启开机自启动"
+sudo systemctl enable docker
 
-# 开启远程控制
+F_log_info "[6] 开启远程控制"
 key__='ExecStart';
 value__='/usr/bin/dockerd -H fd:// -H tcp://0.0.0.0:2375 --containerd=/run/containerd/containerd.sock';
 path__='/lib/systemd/system/docker.service'
 grep "tcp://0.0.0.0:2375" ${path__} || sed -i "s|${key__}=.*$|${key__}=${value__:-""}|g" ${path__};
 
-systemctl daemon-reload
-systemctl restart docker
+F_log_info "[7] 启动docker并校验是否安装成功"
+sudo systemctl start docker
 
 # 确认启动成功
 sudo docker run hello-world;
+$? && F_log_info " --> 启动成功" || Func_log_err " --> 启动失败"
 
 curl http://localhost:2375/version
+$? && F_log_info " --> 远程控制正常" || Func_log_err " --> 远程控制异常"
